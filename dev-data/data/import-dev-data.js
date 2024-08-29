ab119b3b8caa7d61895ba890131d827ce4970441
shadab-iqbal <shadabiqbalmu@gmail.com>
2024-08-29 20:48:10 +0600

    add all data to db

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-console */
 const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Tour = require('./../../models/tourModel');
-
 dotenv.config({ path: './config.env' });
+
+const Tour = require('./../../models/tourModel');
+const User = require('./../../models/userModel');
+const Review = require('./../../models/reviewModel');
 
 mongoose
   .connect(process.env.DATABASE_CONNECTION_STRING)
@@ -11,27 +14,40 @@ mongoose
 
 // READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 // IMPORT DATA INTO DB
 const importData = async () => {
-  try {
-    await Tour.create(tours);
-    console.log('Data successfully loaded!');
-  } catch (err) {
-    console.log(err);
-  }
-  process.exit();
+  const time = Date.now();
+
+  Promise.all([Tour.create(tours), User.create(users), Review.create(reviews)])
+    .then(() => {
+      console.log(`Data successfully loaded in ${Date.now() - time} ms`);
+    })
+    .catch(err => {
+      console.error('Error clearing collections:', err);
+    })
+    .finally(() => {
+      process.exit();
+    });
 };
 
 // DELETE ALL DATA FROM DB
 const deleteData = async () => {
-  try {
-    await Tour.deleteMany();
-    console.log('Data successfully deleted!');
-  } catch (err) {
-    console.log(err);
-  }
-  process.exit();
+  const time = Date.now();
+  Promise.all([Tour.deleteMany(), User.deleteMany(), Review.deleteMany()])
+    .then(() => {
+      console.log(`All data successfully cleared in ${Date.now() - time} ms`);
+    })
+    .catch(err => {
+      console.error('Error clearing collections:', err);
+    })
+    .finally(() => {
+      process.exit();
+    });
 };
 
 if (process.argv[2] === '--import') {
