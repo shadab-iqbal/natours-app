a34842117bbc771fbe026443927b431c1360cd08
shadab-iqbal <shadabiqbalmu@gmail.com>
2024-09-17 11:33:42 +0600

    add check if user is authorized for reviewing

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,7 @@
 const Review = require('../models/reviewModel');
+const Booking = require('../models/bookingModel');
 const controllerFactory = require('../utils/controllerFactory');
+const AppError = require('./../utils/appError');
 
 exports.getAllReviews = controllerFactory.getAll(Review);
 
@@ -21,5 +23,21 @@ exports.setFilterOptions = (req, res, next) => {
 exports.setTourUserIds = (req, res, next) => {
   req.body.reviewFor = req.body.reviewFor || req.params.tourId;
   req.body.reviewBy = req.body.reviewBy || req.user.id;
+  return next();
+};
+
+// Middleware to ensure only an user who has booked the tour can review it
+exports.canReview = async (req, res, next) => {
+  const booking = await Booking.findOne({
+    tour: req.body.reviewFor,
+    user: req.body.reviewBy
+  });
+
+  if (!booking) {
+    return next(
+      new AppError('You need to book this tour before you can review it.', 403)
+    );
+  }
+
   return next();
 };
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -14,6 +14,7 @@ router
   .post(
     authController.isAuthorized(['user']),
     reviewController.setTourUserIds,
+    reviewController.canReview,
     reviewController.createReview
   );
 
